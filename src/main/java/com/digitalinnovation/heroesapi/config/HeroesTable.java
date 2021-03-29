8d8946afd7ef3a5c73cb33989c3b51b5a87053cc
GabrielSantos-dev <53788389+GabrielSantos-dev@users.noreply.github.com>
2021-03-29 09:17:00 -0300

    First Commit

---
+++ src/main/java/com/digitalinnovation/heroesapi/config/HeroesTable.java
@@ -0,0 +1,53 @@
+package com.digitalinnovation.heroesapi.config;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.model.*;
+import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import static com.digitalinnovation.heroesapi.constants.HeroConstant.REGION_DYNAMO;
+import static com.digitalinnovation.heroesapi.constants.HeroConstant.ENDPOINT_DYNAMO;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableDynamoDBRepositories
+
+public class HeroesTable {
+    public static void main(String[] args) throws Exception {
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(ENDPOINT_DYNAMO, REGION_DYNAMO))
+                .build();
+
+        DynamoDB dynamoDB = new DynamoDB(client);
+
+        String tableName = "Heroes_Api_Table";
+
+        try {
+            System.out.println("Criando tabela, aguarde...");
+            Table table = dynamoDB.createTable(tableName,
+                    Arrays.asList(new KeySchemaElement("id", KeyType.HASH)
+                    ),
+                    Arrays.asList(new AttributeDefinition("id", ScalarAttributeType.S)
+
+                    ),
+                    new ProvisionedThroughput(5L, 5L));
+            table.waitForActive();
+            System.out.println("Successo " + table.getDescription().getTableStatus());
+
+        }
+        catch (Exception e) {
+            System.err.println("Não foi possível criar a tabela");
+            System.err.println(e.getMessage());
+        }
+    }
+}
